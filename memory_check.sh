697fc119cc78edf1696b431d0d620b39bf151283
marjpanaligan <marijopanaligan@gmail.com>
2015-09-27 04:28:27 +0800

    Update memory_check.sh

--- memory_check.sh
+++ memory_check.sh
@@ -7,32 +7,36 @@ read c
 echo -e "enter warning threshold"
 read w
 
-declare -i c
-declare -i w
+declare -i critical=c
+declare -i warning=w
 
-function percent {
+#function percent {
 declare -i TOTAL_MEMORY=$( free | grep Mem: |awk '{print $2}')
 declare -i USED_MEMORY=$( free | grep Mem: | awk '{print $3}')
 mem_used=($USED_MEMORY/$TOTAL_MEMORY)
 
 echo "Total memory is: " $TOTAL_MEMORY
 echo "Used memory is " $USED_MEMORY
-awk  'BEGIN{ rounded = sprintf("%.2f", '$mem_used'); percentage = rounded*100; print "Percentage: " percentage"%"}'
-}
-percent
+#awk  'BEGIN{ rounded = sprintf("%.2f", '$mem_used'); percentage = rounded*100; print percentage}'
+#}
 
 
-#awk  'BEGIN{ rounded = sprintf("%.2f", '$mem_used'); percentage = rounded*100; print "Percentage: " percentage"%"}{if ( percentage >= '$c' ) print "2"; else if ( percentage >= '$w' < '$c' ) print "1"; else if ( percentage < '$w' ) print "0";}'
-#echo "scale=2; ${mem_used}"| bc
+#awk  'BEGIN{ rounded = sprintf("%.2f", '$mem_used'); um = rounded*100; print "Percentage: " um"%"}BEGIN{if (um -ge '$critical') print "2"; else if(um -ge '$warning' && -le '$critical') print "1"; else if(um -lt '$warning') print "0";}'
 
-if [ percent >= $c ] ; then
-  echo "2"
-else
-  if [ percent >= $w < $c ] ; then
-    echo "1"
- else
-   if [ percent < $w ] ; then
-     echo "0"
-    fi
-  fi
+mem=$(echo "scale=2; ${mem_used}*100"| bc)
+memu=${mem%.*}
+echo "Percentage is: " $memu"%"
+echo "Crtical threshold: " $critical
+echo "Warning threshold: " $warning
+
+
+if [ ${memu} -ge "$critical" ];
+then
+ echo "Value is 2";
+elif [[ ${memu} -ge "$warning" ]] && [[ "$memu" -lt "$critical" ]] ;
+then
+    echo "Value is 1";
+ elif [ ${memu} -lt "$warning" ]; then
+     echo "Value is 0";
+
 fi
